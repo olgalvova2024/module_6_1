68201b561075b9b5d63fa86d538d78c1c37f6e7a
olgalvova2024 <olga.lvova1317@mail.ru>
2024-08-05 18:29:12 +0300

    Задача "Съедобное, несъедобное"

---
+++ module_6_1.py
@@ -0,0 +1,45 @@
+class Animal:
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+
+class Plant:
+    edible = False
+    def __init__(self, name):
+        self.name = name
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    edible = True
+
+class Mammal(Animal,Plant):
+     def eat(self, food):
+         if food.edible == True:
+             print(f'{self.name} съел {food.name}')
+             self.fed = True
+         else:
+             print(f'{self.name} не стал есть {food.name}')
+             self.alive = False
+
+class Predator(Mammal):
+    pass
+
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
